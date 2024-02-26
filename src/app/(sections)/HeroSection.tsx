5dadf216ebc1e3e15479b63983f6f8273f9ddac7
Abhishek <abhishekbhosale.dev@gmail.com>
2024-02-26 22:49:48 +0530

    feat: hero section

---
+++ src/app/(sections)/HeroSection.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <div className="">
+      <div className="flex flex-col justify-items-center items-center mt-20 ">
+        <h1 className="text-6xl text-center font-semibold">
+          Write, plan, share. <br /> With AI at your side.
+        </h1>
+        <p className="font-medium text-2xl pt-2">
+          Notion is the connected workspace where better, faster work happens.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HeroSection;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Navbar from "@/components/navbar/Navbar";
 import Image from "next/image";
+import HeroSection from "./(sections)/HeroSection";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
